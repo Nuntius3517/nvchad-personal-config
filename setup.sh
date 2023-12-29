5b7984d99e0063f6e34a81cfef064fd6fd79c714
Nuntius3517 <buzz.cyj@gmail.com>
2023-12-29 10:55:33 +0800

    setup.sh change with subcommand install/uninstall

--- setup.sh
+++ setup.sh
@@ -1,19 +1,82 @@
-rm -rf ~/.config/nvim
-rm -rf ~/.local/share/nvim
+#!bin/bash
+# This file is to install personal nvim setting used by the author
+# Author : Github:Nuntius3517
+# Email : buzz.cyj@gmail.com 
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/BigBlueTerminal.zip
-sudo mkdir -p /usr/share/fonts/BigBlueTerm
-sudo unzip -d /usr/share/fonts/BigBlueTerm ./BigBlueTerm.zip
-fc -c -v
-wget https://github.com/neovim/neovim/releases/download/v0.9.4/nvim-linux64.tar.gz
-gunzip nvim-linux64.tar.gz 
-tar xvf nvim-linux64.tar
-sudo cp ./nvim-linux64/bin/nvim /usr/bin/
-sudo cp -r ./nvim-linux64/lib /usr/lib/
-sudo cp -r ./nvim-linux64/share/* /usr/share/
-sudo cp ./nvim-linux64/man/man1/nvim.1 /usr/share/man/man1
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-sudo cp ./init.lua ~/.config/nvim/lua/plugin/init.lua 
-export OPENAI_API_KEY=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
-echo "export OPENAI_API_KEY=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" >> ~/.bashrc
-nvim
+openai_key = "" #Insert your openai key 
+
+
+install() {
+  echo "Installing.... "
+
+  echo "Downloading & Installing the fonts"
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/BigBlueTerminal.zip
+  sudo mkdir -p /usr/share/fonts/BigBlueTerm
+  sudo unzip -d /usr/share/fonts/BigBlueTerm ./BigBlueTerm.zip
+  fc -c -v
+  
+  echo "Downloading & Installing the nvim"
+  wget https://github.com/neovim/neovim/releases/download/v0.9.4/nvim-linux64.tar.gz
+  gunzip nvim-linux64.tar.gz 
+  tar xvf nvim-linux64.tar
+  sudo cp ./nvim-linux64/bin/nvim /usr/bin/
+  sudo cp -r ./nvim-linux64/lib /usr/lib/
+  sudo cp -r ./nvim-linux64/share/* /usr/share/
+  sudo cp ./nvim-linux64/man/man1/nvim.1 /usr/share/man/man1/
+  sudo cp ./nvim-linux64/share/icons/hicolor/128x128/apps/nvim.png /usr/share/icons/hicolor/128x128/apps 
+  sudo cp -r ./nvim-linux64/share/nvim/ /usr/share/ 
+  sudo cp -r ./nvim-linux64/share/locale /usr/share/locale/ 
+
+  echo "Downloading & Installing NvChad"
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+  echo "Insatlling config and enviroment variable"
+  sudo cp ./init.lua ~/.config/nvim/lua/plugins  
+  export OPENAI_API_KEY=$(openai_key)
+  echo "export OPENAI_API_KEY=$(openai_key)" >> ~/.bashrc
+  echo "alias vim=\"nvim\""
+
+  nvim 
+}
+
+
+uninstall() {
+  echo "Removing........."
+  echo "Removing the fonts"
+  sudo rm -rf /usr/share/fonts/BigBlueTerminal
+  
+  echo "Removing the nvim"
+  sudo rm -rf /usr/bin/nvim
+  sudo rm -rf /usr/lib/nvim
+  sudo rm /usr/share/man/man1/nvim.1 
+  sudo rm -rf /usr/share/nvim 
+  sudo rm /usr/share/icons/hicolor/128x128/apps/nvim.png
+
+  echo "Removing the NvChad"
+  sudo rm -rf ~/.config/nvim/
+ 
+  echo "Removing the OPENAI_API_KEY"
+  sed -i "/export OPENAI_API_KEY=$(openai_key)" ~/.bashrc
+  sed -i "/alias vim=\"nvim\"/d" ~/.bashrc
+
+}
+
+
+if [ $# -eq 0 ]; then 
+    echo "No arguments provided"
+else
+  case "$1" in 
+    install)
+      install
+      ;;
+    uninstall)
+      uninstall
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      echo "Usage: $0 {install|uninsatll}"
+      exit 1
+  esac 
+fi 
+      
+
